966b9272bb0f441e8ff2912b3baea12f9f53a68a
Filipe Assunção <assuncaofilipe97@gmail.com>
2021-06-17 11:21:37 -0300

    Update index.php

--- project/index.php
+++ project/index.php
@@ -15,27 +15,99 @@ use App\Models\Product\Product;
 use App\Models\Order\Order;
 use Faker\Factory;
 
+    $faker = Factory::create('pt_BR');
 
-$faker = Factory::create('pt_BR');
+    $adress = new Adress(
+        $faker->streetName,
+        $faker->postcode, 
+        'SC', 
+        $faker->city
+    );
 
-$adress = new Adress($faker->streetName, $faker->postcode, 'SC', $faker->city);
-$customer = new Customer($faker->name, $faker->email, $faker->cpf, '48920006356', $adress, $faker->date(), new \DateTime('2020-06-03'));
-$store = new Store($faker->name, $faker->email, $faker->cnpj, '48920006589', $adress, 'software', new \DateTime('2020-06-03'));
+    $customer = new Customer(
+        $faker->name, 
+        $faker->email,
+        $faker->cpf, 
+        '48920006356',
+        $adress, 
+        $faker->date(),
+        new \DateTime('2020-06-03')
+    );
 
-$shippingCompany = new ShippingCompany('FASTTRANSPORTES', $faker->email, $faker->cnpj, '48920006589', $adress, 'software', new \DateTime('2020-06-03'), 13.55, 300);
+    $store = new Store(
+        $faker->name,
+        $faker->email, 
+        $faker->cnpj, 
+        '48920006589',
+        $adress, 
+        'software', 
+        new \DateTime('2020-06-03')
+    );
 
-$products = new Collection(new Product(5554, 'binquedo', 256, 25, 100, 2500));
-$products->add(new Product(5554, 'binquedo', 256, 25, 100, 2500));
+    $shippingCompany = new ShippingCompany(
+        'FASTTRANSPORTES', 
+        $faker->email, 
+        $faker->cnpj, 
+        '48920006589',
+        $adress, 
+        'software', 
+        new \DateTime('2020-06-03'), 
+        13.55, 
+        300
+    );
 
-$order = new Order('135980', $store, $customer, $shippingCompany, $products);
+    $products = new Collection(
+        new Product(
+        5554, 
+        'binquedo', 
+        256, 
+        25, 
+        100, 
+        2500
+        )
+    );
 
-$payment = new Payment();
-$cielo = new Cielo();
-$fpay = new FPay();
+    $products->add(
+        new Product(
+            5554, 
+            'brinquedo', 
+            256, 
+            25, 
+            100, 
+            2500
+        )
+    );
 
-$creditCard = new CreditCard('5159-2673-6337-7964', 'Teste', '09/2028', '122', 'master');
+    $order = new Order(
+        '135980',
+        $store,
+        $customer,
+        $shippingCompany,
+        $products
+    );
 
-$paymentReturn = $payment->payCreditCard($cielo, $order->merchantOrderId, $customer, 50000, $creditCard, 1);
+    $payment = new Payment();
+    $cielo = new Cielo();
+    $fpay = new FPay();
+
+    $creditCard = new CreditCard(
+        '5159-2673-6337-7964', 
+        'Teste', 
+        '09/2028', 
+        '122', 
+        'master', 
+        true
+    );
+
+    $paymentReturn = $payment->payCreditCard(
+        $cielo, 
+        $order->merchantOrderId,
+        $customer, 
+        50000, 
+        $creditCard, 
+        1
+    );
+    
 echo "PAYMENT CIELO \n" . $paymentReturn . "\n\n";
 
 $paymentId = json_decode($paymentReturn, true)["Payment"]["PaymentId"];
@@ -44,7 +116,7 @@ echo "GET PAYMENT CIELO \n" . $payment->getTransaction($cielo, $paymentId) . "\n
 
 echo "REVERSE PAYMENT CIELO \n" . $payment->reverseTransaction('PUT', $cielo, $paymentId) . "\n\n";
 
-$creditCard = new CreditCard('5159267363377964', 'Teste', '11/28', '122', 'master');
+$creditCard = new CreditCard('5159267363377964', 'Teste', '11/28', '122', 'master', true);
 
 $paymentReturn = $payment->payCreditCard($fpay, $order->merchantOrderId, $customer, 50.00, $creditCard, 1);
 
